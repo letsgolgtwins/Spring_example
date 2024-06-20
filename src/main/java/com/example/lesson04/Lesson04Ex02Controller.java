5a9ccddefcf53c25b437208303ad268dc4d53044
LeeMinHee <98minh2@gmail.com>
2024-06-20 12:29:20 +0900

    240620 변경

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.lesson04.BO.StudentBO;
 import com.example.lesson04.domain.Student;
